facbae3cc61f30c3b970358bd5d5d34bcfe18353
Trafi073 <d.trafian01@ufromail.cl>
2021-11-14 22:06:07 -0300

    Encabezados de funciones completas en implementaciones de las mismas (libreriaHorarios.cpp)

--- libreriaHorarios.cpp
+++ libreriaHorarios.cpp
@@ -6,6 +6,15 @@
 // Implementaciones
 
 void implementacionLista(int *opcion){
+  /*
+  * Nombre de la función: implementacionLista.
+  * Tipo de función: void.
+  * Parámetros: "opcion" de tipo int.
+  * 
+  * Dato de retorno: ---------------------
+  * Descripción de la función: La función implementacionLista () muestra por pantalla la diferentes opciones 
+  *                            que el usuario tiene disponible para realizar.
+  */
   do{
     printf("\n--------------------------------\n");
     printf("------ CLAVISTEL EMPRESAS ------\n");
@@ -36,6 +45,15 @@ void CantidadTotalFuncionarios(int* Funcionarios){
 }
 
 void Presentacion(){
+  /*
+  * Nombre de la función: Presentacion.
+  * Tipo de función: void.
+  * Parámetros: ------------------
+  * 
+  * Dato de retorno: ---------------------
+  * Descripción de la función: La función Presentacion () le muestra por pantalla una bienvenida al usuario,
+  *                            a la interfaz de clavistel.
+  */
   printf("\n--------------------------------\n");
   printf("------ CLAVISTEL EMPRESAS ------\n");
   printf("--------------------------------\n");
@@ -43,6 +61,14 @@ void Presentacion(){
 }
 
 void nuevoIngresoTrabajador(Lista &lista){
+  /* Nombre de la función: nuevoIngresoTrabajador.
+  * Tipo de función: void.
+  * Parámetros: "lista” de tipo Lista.
+  * 
+  * Dato de retorno: ---------------------
+  * Descripción de la función: La función nuevoIngresoTrabajador() solicita al trabajador, 
+  *                            ingresar sus datos personales. 
+  */
   trabajador nuevoTrabajador;
   // Ingreso de Datos
   printf("Ingrese su Nombre y sus 2 Apellidos:\n");
@@ -60,13 +86,30 @@ void nuevoIngresoTrabajador(Lista &lista){
 }
 
 void DatosTrabajadorIngresado(Lista lista){
+  /*
+  * Nombre de la función: DatosTrabajadorIngresado.
+  * Tipo de función: void.
+  * Parámetros: "lista” de tipo Lista.
+  * 
+  * Dato de retorno: ---------------------
+  * Descripción de la función: La función DatosTrabajadorIngresado() 
+  *                            recupera los elementos anteriores al fin de la lista. 
+  */
   tipoDato x = recupera(anterior(fin(lista), lista), lista);
   // Impresión por pantalla de los datos ingresados.
   printf("\n- Nombre   : %s. \n- Apellidos: %s %s.\n- RUN      : %s \n- Telefono : %lu.\n- Correo   : %s \n", x.nombre, x.primerApellido, x.segundoApellido, x.run, x.telefono, x.correo);
 }
 
 void ConfirmarIngresoDeDatos(int *opcion){
-  // Confirmación de datos.
+  /*
+  * Nombre de la función: ConfirmarIngresoDeDatos.
+  * Tipo de función: void.
+  * Parámetros: "opcion” de tipo int.
+  * 
+  * Dato de retorno: ---------------------
+  * Descripción de la función: La función ConfirmarIngresoDeDatos() consulta al funcionario verificar 
+  *                            si sus datos ingresados son correctos o necesita corregir alguno. 
+  */
   do{
     printf("\n--------------------------------------------------------------------------------------------\n");
     printf("| Los datos ingresados anteriormente estan completamente en orden o desea reingresarlos?: |\n");
@@ -80,6 +123,19 @@ void ConfirmarIngresoDeDatos(int *opcion){
 }
 
 void TableroResumenSemanal(Horario LunesEntrada, Horario LunesSalida, Horario MartesEntrada, Horario MartesSalida, Horario MiercolesEntrada, Horario MiercolesSalida, Horario JuevesEntrada, Horario JuevesSalida, Horario ViernesEntrada, Horario ViernesSalida, Horario SabadoEntrada, Horario SabadoSalida){
+  /*
+  * Nombre de la función: TableroResumenSemanal.
+  * Tipo de función: void.
+  * Parámetros: "LunesEntrada, LunesSalida" de tipo Struct_Horario.
+  *             "MartesEntrada, MartesSalida" de tipo Struct_Horario.
+  *             "MiercolesEntrada, MiercolesSalida" de tipo Struct_Horario.
+  *             "JuevesEntrada, JuevesSalida" de tipo Struct_Horario.
+  *             "ViernesEntrada, ViernesSalida" de tipo Struct_Horario.
+  *             "SabadoEntrada, SabadoSalida" de tipo Struct_Horario.
+  * 
+  * Dato de retorno: ---------------------
+  * Descripción de la función: La función TableroResumenSemanal() muestra un tablero en pantalla con los horarios de entrada y salida de cada jornada.
+  */
   printf("\n                 |   Lunes    |   Martes    |     Miercoles    |     Jueves     |     Viernes     |     Sabado  \n");
   printf("-------------------------------------------------------------------------------------------------------------------\n");
   printf(" Hora de Ingreso |   %d:%d           %d:%d            %d:%d               %d:%d              %d:%d               %d:%d   \n", LunesEntrada.hora, LunesEntrada.minutos, MartesEntrada.hora, MartesEntrada.minutos, MiercolesEntrada.hora, MiercolesEntrada.minutos, JuevesEntrada.hora, JuevesEntrada.minutos, ViernesEntrada.hora, ViernesEntrada.minutos, SabadoEntrada.hora, SabadoEntrada.minutos);
@@ -89,6 +145,16 @@ void TableroResumenSemanal(Horario LunesEntrada, Horario LunesSalida, Horario Ma
 }
 
 int calcularCantMinutosLunes(Horario* Entrada, Horario* Salida){
+  /*
+  * Nombre de la función: calcularCantMinutosLunes.
+  * Tipo de función: int.
+  * Parámetros: "Entrada” de tipo Struct_Horario.
+  *             "Salida” de tipo Struct_Horario.
+  * 
+  * Dato de retorno: "TotalMinutos" de tipo int.
+  * Descripción de la función: La función calcularCantMinutosLunes() muestra por pantalla al usuario el ingreso y salida, 
+  *                            para luego calcular y retornar los minutos trabajados el dia lunes.
+  */
   printf("\nA continuacion digite el horario en el que hizo entrada su turno el dia lunes en formato 24 horas: (hh:mm)\n");
   scanf("%d:%d", &(Entrada->hora), &(Entrada->minutos));
   printf("A continuacion digite el horario en el que hizo termino su turno el dia lunes en formato 24 horas: (hh:mm)\n");
@@ -119,6 +185,16 @@ int calcularCantMinutosLunes(Horario* Entrada, Horario* Salida){
 }
 
 int calcularCantMinutosMartes(Horario* Entrada, Horario* Salida){
+  /*
+  * Nombre de la función: calcularCantMinutosMartes.
+  * Tipo de función: int.
+  * Parámetros: "Entrada” de tipo Struct_Horario.
+  *             "Salida” de tipo Struct_Horario.
+  * 
+  * Dato de retorno: "TotalMinutos" de tipo int.
+  * Descripción de la función: La función calcularCantMinutosLunes() muestra por pantalla al usuario el ingreso y salida, 
+  *                            para luego calcular y retornar los minutos trabajados el dia martes.
+  */
   printf("A continuacion digite el horario en el que hizo entrada su turno el dia martes en formato 24 horas: (hh:mm)\n");
   scanf("%d:%d", &(Entrada->hora), &(Entrada->minutos));
   printf("A continuacion digite el horario en el que hizo termino su turno el dia martes en formato 24 horas: (hh:mm)\n");
@@ -147,6 +223,16 @@ int calcularCantMinutosMartes(Horario* Entrada, Horario* Salida){
 }
 
 int calcularCantMinutosMiercoles(Horario* Entrada, Horario* Salida){
+  /*
+  * Nombre de la función: calcularCantMinutosMiercoles.
+  * Tipo de función: int.
+  * Parámetros: "Entrada” de tipo Struct_Horario.
+  *             "Salida” de tipo Struct_Horario.
+  * 
+  * Dato de retorno: "TotalMinutos" de tipo int.
+  * Descripción de la función: La función calcularCantMinutosLunes() muestra por pantalla al usuario el ingreso y salida, 
+  *                            para luego calcular y retornar los minutos trabajados el dia miércoles.
+  */
   printf("A continuacion digite el horario en el que hizo entrada su turno el dia miercoles en formato 24 horas: (hh:mm)\n");
   scanf("%d:%d", &(Entrada->hora), &(Entrada->minutos));
   printf("A continuacion digite el horario en el que hizo termino su turno el dia miercoles en formato 24 horas: (hh:mm)\n");
@@ -175,6 +261,16 @@ int calcularCantMinutosMiercoles(Horario* Entrada, Horario* Salida){
 }
 
 int calcularCantMinutosJueves(Horario* Entrada, Horario* Salida){
+  /*
+  * Nombre de la función: calcularCantMinutosJueves.
+  * Tipo de función: int.
+  * Parámetros: "Entrada” de tipo Struct_Horario.
+  *             "Salida” de tipo Struct_Horario.
+  * 
+  * Dato de retorno: "TotalMinutos" de tipo int.
+  * Descripción de la función: La función calcularCantMinutosLunes() muestra por pantalla al usuario el ingreso y salida, 
+  *                            para luego calcular y retornar los minutos trabajados el dia jueves.
+  */
   printf("A continuacion digite el horario en el que hizo entrada su turno el dia jueves en formato 24 horas: (hh:mm)\n");
   scanf("%d:%d", &(Entrada->hora), &(Entrada->minutos));
   printf("A continuacion digite el horario en el que hizo termino su turno el dia jueves en formato 24 horas: (hh:mm)\n");
@@ -203,6 +299,16 @@ int calcularCantMinutosJueves(Horario* Entrada, Horario* Salida){
 }
 
 int calcularCantMinutosViernes(Horario* Entrada, Horario* Salida){
+  /*
+  * Nombre de la función: calcularCantMinutosViernes.
+  * Tipo de función: int.
+  * Parámetros: "Entrada” de tipo Struct_Horario.
+  *             "Salida” de tipo Struct_Horario.
+  * 
+  * Dato de retorno: "TotalMinutos" de tipo int.
+  * Descripción de la función: La función calcularCantMinutosLunes() muestra por pantalla al usuario el ingreso y salida, 
+  *                            para luego calcular y retornar los minutos trabajados el dia viernes.
+  */
   printf("A continuacion digite el horario en el que hizo entrada su turno el dia viernes en formato 24 horas: (hh:mm)\n");
   scanf("%d:%d", &(Entrada->hora), &(Entrada->minutos));
   printf("A continuacion digite el horario en el que hizo termino su turno el dia viernes en formato 24 horas: (hh:mm)\n");
@@ -231,6 +337,16 @@ int calcularCantMinutosViernes(Horario* Entrada, Horario* Salida){
 }
 
 int calcularCantMinutosSabado(Horario* Entrada, Horario* Salida){
+  /*
+  * Nombre de la función: calcularCantMinutosSabado.
+  * Tipo de función: int.
+  * Parámetros: "Entrada” de tipo Struct_Horario.
+  *             "Salida” de tipo Struct_Horario.
+  * 
+  * Dato de retorno: "TotalMinutos" de tipo int.
+  * Descripción de la función: La función calcularCantMinutosLunes() muestra por pantalla al usuario el ingreso y salida, 
+  *                            para luego calcular y retornar los minutos trabajados el dia sábado.
+  */
   printf("A continuacion digite el horario en el que hizo entrada su turno el dia sabado en formato 24 horas: (hh:mm)\n");
   scanf("%d:%d", &(Entrada->hora), &(Entrada->minutos));
   printf("A continuacion digite el horario en el que hizo termino su turno el dia sabado en formato 24 horas: (hh:mm)\n");
@@ -259,21 +375,67 @@ int calcularCantMinutosSabado(Horario* Entrada, Horario* Salida){
 }
 
 void imprimirTotalMinutos(int totalMinCalculados){
+  /*
+  * Nombre de la función: imprimirTotalMinutos.
+  * Tipo de función: void.
+  * Parámetros: "totalMinCalculados” de tipo int.
+  * 
+  * Dato de retorno: ---------------------
+  * Descripción de la función: La función imprimirTotalMinutos() imprime por pantalla el total de  los minutos trabajados.
+  */
     printf("El total de los minutos es: %d\n\n", totalMinCalculados);
 }
 
 // Cálculo de la remuneración semanal con respecto a los minutos calculados previamente por día de Lunes a Sábado.
 int calcularSueldoSemanal(int cantMinutos1, int cantMinutos2, int cantMinutos3, int cantMinutos4, int cantMinutos5, int cantMinutos6){
+  /*
+  * Nombre de la función: calcularSueldoSemanal.
+  * Tipo de función: int.
+  * Parámetros: "cantMinutos1” de tipo int.
+  *             "cantMinutos2” de tipo int.
+  *             "cantMinutos3” de tipo int.
+  *             "cantMinutos4” de tipo int.
+  *             "cantMinutos5” de tipo int.
+  *             "cantMinutos6” de tipo int.
+  * 
+  * Dato de retorno: sueldoSemanalFuncionario de tipo int.
+  * Descripción de la función: La función calcularSueldoSemanal() calcula el sueldo por el dia y luego lo suma para dar un monto final, para dar un retorno de tipo entero.
+  */
   int sueldoSemanalFuncionario = (cantMinutos1 + cantMinutos2 + cantMinutos3 + cantMinutos4 + cantMinutos5 + cantMinutos6) * 40;
   return sueldoSemanalFuncionario;
 }
 
 void imprimirSueldoSemanal(int sueldoCalculado){
+  /*
+  * Nombre de la función: imprimirSueldoSemanal.
+  * Tipo de función: void.
+  * Parámetros: "sueldoCalculado” de tipo int.
+  * 
+  * Dato de retorno: ---------------------
+  * Descripción de la función: La función imprimirSueldoSemanal() muestra por pantalla el sueldo por honorarios de la semana correspondiente.
+  */
     printf("El sueldo por honorarios correspondientes a esta semana es de: $%d pesos.\n\n", sueldoCalculado);
 }
 
 // Resumen y evaluación final que tiene el fin de presentarle un informe completo de acuerdo a las exigencias minimas de la empresa. 
 void ResumenAGerente(Lista lista, FILE* archivo, int minutosLunes, int minutosMartes, int minutosMiercoles, int minutosJueves, int minutosViernes, int minutosSabado){
+  /*
+  * Nombre de la función: ResumenAGerente.
+  * Tipo de función: void.
+  * Parámetros: "lista" de tipo Lista.
+  *             "archivo" de tipo FILE.
+  *             "minutosLunes” de tipo int.
+  *             "minutosMartes” de tipo int.
+  *             "minutosMiercoles” de tipo int.
+  *             "minutosJueves” de tipo int.
+  *             "minutosViernes” de tipo int.
+  *             "minutosSabado” de tipo int.
+  * 
+  * Dato de retorno: ---------------------
+  * Descripción de la función: La función ResumenAGerente(), a partir del último trabajador ingresado y los horarios que haya ingresado,
+  *                            calcula y muestra por pantalla el sueldo y los minutos trabajados totales por semana,
+  *                            con el fin de evaluar si el trabajador cumplio con lo mínimo establecido.
+  */
   tipoDato x = recupera(anterior(fin(lista), lista), lista);
 
   fprintf(archivo,"-----------------------------------------------------\n");
@@ -305,7 +467,17 @@ void ResumenAGerente(Lista lista, FILE* archivo, int minutosLunes, int minutosMa
 }
 
 void opcionesManejoDLista(Lista &lista, int *opcion, int *numeroFuncionario){
-  
+  /*
+  * Nombre de la función: opcionesManejoDLista.
+  * Tipo de función: void.
+  * Parámetros: "lista" de tipo Lista.
+  *             "opcion” de tipo int.
+  *             "numeroFuncionario" de tipo int.
+  * 
+  * Dato de retorno: ---------------------
+  * Descripción de la función: La función opcionesManejoDLista() muestra por pantalla las diferentes opciones a realizar con la lista,
+  *                            en caso de necesitar revisarla o modificarla.
+  */
   do{
     printf("\nQue operacion se realizara?\nIndique la opcion:\n");
     printf("\n  1. Eliminar funcionario de la lista.");
